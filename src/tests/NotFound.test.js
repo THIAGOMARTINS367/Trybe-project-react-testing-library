2e738ee7733ce43d7ce4776d97b8fd43b40ff266
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-01-28 20:05:27 -0300

    Requisito 4 feito.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+import { element } from 'prop-types';
+
+
+it('Testa se página contém um heading h2\n com o texto: "Page requested not found 😭".', () => {
+  const { getByRole, getAllByRole } = renderWithRouter(<NotFound />);
+  const headingNotFound = getByRole('heading');
+  const imgNotFound = getAllByRole('img');
+  let srcImgNotFound = '';
+  imgNotFound.map((element) => {
+    if (element.src !== undefined) {
+      srcImgNotFound = element.src;
+    }
+  });
+  expect(headingNotFound.textContent).toBe('Page requested not found 😭');
+  expect(srcImgNotFound).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
